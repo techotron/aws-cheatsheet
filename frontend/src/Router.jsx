41eced4cea0636465ff30d3476d6a4cfa7135920
esnow <edward.snow@disneystreaming.com>
2020-12-07 22:50:23 +0000

    move code around

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route } from 'react-router-dom';
+import { Container } from '@material-ui/core';
+import App from './App';
 
 
 
@@ -10,16 +12,17 @@ function Router() {
                 <Route exact
                     path='/'
                     render={() =>
-                        <span>
+                        <Container style={{fontFamily: "sans-serif"}}>
+                            <App />
                             <h1>This is the homepage</h1>
-                        </span>
+                        </Container>
                     }
                 />
                 <Route exact
                     path='/info'
                     render={() =>
                         <span>
-                            <h1>Welcome to AWS Cheatsheet</h1>
+                            <h1>Welcome to AWS Cheatsheet info page</h1>
                             Example of dynamic env var:
                             <h3>API_URL: {window.env.REACT_APP_API_URL}</h3>
                         </span>
