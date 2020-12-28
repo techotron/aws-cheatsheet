3ed84f6ad8a685e267c951ab76b475924d732713
esnow <edward.snow@disneystreaming.com>
2020-12-28 22:43:37 +0000

    fix linting complaints

--- frontend/src/Templates/CategoryData.js
+++ frontend/src/Templates/CategoryData.js
@@ -4,13 +4,13 @@ import getMockStorageService from '../Mocks/MockStorageService'
 import getMockSecurityService from '../Mocks/MockSecurityService'
 
 function getCategoryData (category, service) {
-  if (category == 1) {
+  if (category === '1') {
     return getMockComputeService(service)
-  } else if (category == 2) {
+  } else if (category === '2') {
     return getMockStorageService(service)
-  } else if (category == 3) {
+  } else if (category === '3') {
     return getMockDatabaseService(service)
-  } else if (category == 4) {
+  } else if (category === '4') {
     return getMockSecurityService(service)
   }
 }
