043d13024c3569ca010c80d34236d03f7e118a6f
esnow <edward.snow@disneystreaming.com>
2020-12-28 22:28:17 +0000

    fix linting complaints

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,13 @@
-let express = require('express')
-let cors = require('cors')
-let app = express()
+const express = require('express')
+const cors = require('cors')
+const app = express()
 
 app.use(cors())
 
 app.get('/', (req, res, next) => {
-    res.json('Hello from the backend')
+  res.json('Hello from the backend')
 })
 
 app.listen(5000, () => {
-    console.log('Server running on port 5000')
+  console.log('Server running on port 5000')
 })
