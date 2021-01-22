47a8334cd6da1c6e1df6f3e3af152fcc03b466fb
esnow <edward.snow@disneystreaming.com>
2021-01-22 22:13:43 +0000

    restructure tables and remove those not yet needed

--- backend/migration/0001_add_aws_revision_tables.sql
+++ backend/migration/0001_add_aws_revision_tables.sql
@@ -6,7 +6,8 @@ CREATE TABLE IF NOT EXISTS category_types(
 CREATE TABLE IF NOT EXISTS categories(
     category_id INT PRIMARY KEY,
     type_id INT NOT NULL,
-    category_name VARCHAR(50) UNIQUE NOT NULL,
+    category_title VARCHAR(50) NOT NULL,
+    category_name VARCHAR(50) NOT NULL,
     CONSTRAINT fk_type_in_category_types FOREIGN KEY(type_id) REFERENCES category_types(category_type_id)
 );
 
@@ -15,6 +16,7 @@ CREATE TABLE IF NOT EXISTS sub_categories(
     category_id INT NOT NULL,
     sub_category_name VARCHAR(50) UNIQUE NOT NULL,
     sub_category_title VARCHAR(50) UNIQUE NOT NULL,
+    sub_category_icon VARCHAR(20) NOT NULL,
     sub_items INT[],
     CONSTRAINT fk_subcategory_in_category FOREIGN KEY(category_id) REFERENCES categories(category_id)
 );
@@ -29,19 +31,5 @@ CREATE TABLE IF NOT EXISTS sub_items(
 CREATE TABLE IF NOT EXISTS summaries(
     summary_id INT PRIMARY KEY,
     summary TEXT NOT NULL,
-    use_case TEXT NOT NULL,
     CONSTRAINT fk_subcategory_in_subcategory FOREIGN KEY(summary_id) REFERENCES sub_categories(sub_category_id)
 );
-
-CREATE TABLE IF NOT EXISTS checklists(
-    checklist_id INT PRIMARY KEY,
-    checklist TEXT NOT NULL,
-    CONSTRAINT fk_subcategory_in_subcategory FOREIGN KEY(checklist_id) REFERENCES sub_categories(sub_category_id)
-);
-
-CREATE TABLE IF NOT EXISTS pricing_models(
-    pricing_model_id INT PRIMARY KEY,
-    pricing_model TEXT NOT NULL,
-    CONSTRAINT fk_subcategory_in_subcategory FOREIGN KEY(pricing_model_id) REFERENCES sub_categories(sub_category_id)
-);
-
