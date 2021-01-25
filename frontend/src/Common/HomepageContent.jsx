ccef89bf3138d114eca8aa7cf4b1258457b18eed
esnow <edward.snow@disneystreaming.com>
2021-01-25 18:16:01 +0000

    update homepage

--- frontend/src/Common/HomepageContent.jsx
+++ frontend/src/Common/HomepageContent.jsx
@@ -5,6 +5,6 @@ A site for learning about AWS services.
 
 Primarily so I can learn how to use Javascript/Node whilst also revising for an AWS Certification :)
 
-Application can be found on [Github](https://github.com/techotron/sre-resources)`
+The source code for the application can be found on [Github](https://github.com/techotron/sre-resources)`
     )
 }
