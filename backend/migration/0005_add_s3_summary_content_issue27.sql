5d938d05c8effc1e148717757abb812e5aa2f4d6
esnow <edward.snow@disneystreaming.com>
2021-02-19 20:46:24 +0000

    add initial code

---
+++ backend/migration/0005_add_s3_summary_content_issue27.sql
@@ -0,0 +1,9 @@
+-- Add sub categories
+INSERT INTO sub_categories (category_id, sub_category_name, sub_category_title, sub_category_icon)
+VALUES
+    (1, 's3glacierselect', 'S3 Select and Glacier Select', 's3');
+
+-- Add summaries
+INSERT INTO summaries (summary)
+VALUES
+    ('');
