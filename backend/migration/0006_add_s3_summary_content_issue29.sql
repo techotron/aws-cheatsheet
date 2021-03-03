033a8cf90abb6032f419ca1bad18d837d760f2db
esnow <edward.snow@disneystreaming.com>
2021-03-03 17:46:31 +0000

    initial content

---
+++ backend/migration/0006_add_s3_summary_content_issue29.sql
@@ -0,0 +1,15 @@
+-- Add sub categories
+INSERT INTO sub_categories (category_id, sub_category_name, sub_category_title, sub_category_icon)
+VALUES
+    (1, 's3storagegw', 'Storage Gateway', 's3'),
+    (1, 's3athena', 'Athena vs. Macie', 's3'),
+    (1, 's3summary', 'S3 and IAM Summary', 's3');
+
+-- Add summaries
+INSERT INTO summaries (summary)
+VALUES
+    (''),
+
+    (''),
+
+    ('');
