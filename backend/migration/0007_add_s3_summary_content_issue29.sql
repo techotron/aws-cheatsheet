794065059feb80b85bc71b875120dd4d01071231
esnow <edward.snow@disneystreaming.com>
2021-03-03 18:55:46 +0000

    add course content

--- backend/migration/0007_add_s3_summary_content_issue29.sql
+++ backend/migration/0007_add_s3_summary_content_issue29.sql
@@ -1,8 +1,8 @@
 -- Add sub categories
 INSERT INTO sub_categories (category_id, sub_category_name, sub_category_title, sub_category_icon)
 VALUES
-    (1, 's3storagegw', 'Storage Gateway', 's3'),
-    (1, 's3athena', 'Athena vs. Macie', 's3'),
+    (1, 's3storagegw', 'Storage Gateway', 'storagegw'),
+    (1, 's3athena', 'Athena vs. Macie', 'athena'),
     (1, 's3summary', 'S3 and IAM Summary', 's3');
 
 -- Add summaries
