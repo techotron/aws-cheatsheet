ee73ade225e8d5addf097368ddec3ff6e777b4a2
esnow <edward.snow@disneystreaming.com>
2021-03-04 17:27:08 +0000

    change icon for sub category

--- backend/migration/0005_add_s3_summary_content_issue27.sql
+++ backend/migration/0005_add_s3_summary_content_issue27.sql
@@ -1,7 +1,7 @@
 -- Add sub categories
 INSERT INTO sub_categories (category_id, sub_category_name, sub_category_title, sub_category_icon)
 VALUES
-    (1, 's3glacierselect', 'S3 Select and Glacier Select', 's3'),
+    (1, 's3glacierselect', 'S3 Select and Glacier Select', 's3glacier'),
     (1, 'billingawsorgs', 'AWS Organizations and Consolidated Billing', 'billing'),
     (1, 's3crossaccounts', 'Sharing S3 Buckets Across Accounts', 's3'),
     (1, 's3crossregionreplication', 'Cross-Region Replication', 's3'),
